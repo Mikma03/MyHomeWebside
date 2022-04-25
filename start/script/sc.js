f8d4e9ffb8024c0911cf24df43773b4a006f73c1
thePariti <jajakub4@gmail.com>
2022-04-25 23:51:52 +0200

    burger

--- start/script/sc.js
+++ start/script/sc.js
@@ -1,4 +1,21 @@
 
+
+const burger = document.querySelector(".burger");
+
+const iconBurger = document.querySelector(".fa-bars");
+const iconX = document.querySelector(".fa-xmark");
+const column = document.querySelector("aside");
+
+burger.addEventListener("click", function () {
+    iconBurger.classList.toggle("show");
+    iconX.classList.toggle("show");
+    column.classList.toggle("show");
+
+})
+
+
+
+
 $('nav a').on('click', function () {
     const goToSection = "[data-section=" + $(this).attr('class') + "]";
     $('body, html').animate({
