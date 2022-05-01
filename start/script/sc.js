3bcff5346d44bbe25c46ce1aff6002cc2c8a74a7
thePariti <jajakub4@gmail.com>
2022-05-01 22:19:43 +0200

    karuzela

--- start/script/sc.js
+++ start/script/sc.js
@@ -23,4 +23,10 @@ $('nav a').on('click', function () {
     })
 })
 
-
+new Splide( '.splide' ).mount();
+var splide = new Splide( '.splide', {
+type   : 'loop',
+perPage: 3,
+perMove: 1,
+} );
+splide.mount();
